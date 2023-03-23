7527244396c069ea09fafcc96546f02a3ff6b5d0
Siddharth Sharma <ss739@snu.edu.in>
2023-03-23 17:01:16 +0530

    Added physical world stage

--- simulate.py
+++ simulate.py
@@ -1,4 +1,11 @@
+import time as t
 import pybullet as p
 physicsClient = p.connect(p.GUI)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for i in range(0, 1000):
+    p.stepSimulation()
+    t.sleep(0.01)
+    print(i)
 
 p.disconnect()
